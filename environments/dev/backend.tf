9b5ff8afed7f42d4088c5e720f58462c5b14198a
ghari2krishna-beep <g.hari2krishna@gmail.com>
2026-05-26 10:54:45 +0530

    fixed backend file

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket         = "terraform-state-hari1"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-  }
+   }
 }
