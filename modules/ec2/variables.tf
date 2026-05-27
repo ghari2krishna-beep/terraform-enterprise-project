b463f81790b666f0efcb550d3af7e794ac8feee2
ghari2krishna-beep <g.hari2krishna@gmail.com>
2026-05-27 15:21:09 +0530

    variables changes

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,4 +1,8 @@
-variable "ami_id" {}
+variable "ami_id" {
+  description = "EC2 AMI ID"
+  type        = string
+  default     = "ami-0f58b397bc5c1f2e8"
+}
 variable "instance_type" {}
 variable "subnet_id" {}
 variable "sg_id" {}
